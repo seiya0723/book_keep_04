845bb428fd91576ad24500aeee244bdc08485e90
Seiya Asahina <seiya@asahina>
2021-12-14 17:21:04 +0900

    add

---
+++ finance/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name    = "finance"
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('category/',views.category,name="category")
+]
